edf38abd90b7a6272fe01c7087f691990fe98aba
timr <timrcode0@gmail.com>
2023-03-29 17:46:13 +0200

    fix

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -4,7 +4,7 @@ let currentScrollPos = window.pageYOffset;
   if (prevScrollpos > currentScrollPos) {
     document.getElementById("header").style.top = "0";
   } else {
-    document.getElementById("header").style.top = "-72px";
+    document.getElementById("header").style.top = "-52px";
   }
   prevScrollpos = currentScrollPos;
 }
